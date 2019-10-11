1798022f090542f603cb5fa48e1121f349f734f6
Timothy Oliver <oliverwakogi@live.com>
2019-10-11 16:20:19 +0300

    Refactoring code

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
+from config import config_options
 
-# initializing application
-app = Flask(__name__, instance_relative_config=True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+  app = Flask(__name__)
 
-#initializing Flask Extensions
-bootstrap = Bootstrap(app)
+  #Creating the app configurations
+  app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+  #Initializing flask extensions
+  bootstrap.init_app(app)
+
+  #will add the views and forms
+
+  # Registering the blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  # setting config
+  from .requests import configure_request
+  configure_request(app)
+
+  return app
--- app/requests.py
+++ app/requests.py
@@ -1,14 +1,15 @@
-from app import app
 import urllib.request, json
-from .models import movie
+from .models import Movie
 
 # getting api key
-api_key = app.config['MOVIE_API_KEY']
-
+api_key = None
 # getting the movie base url
-base_url = app.config['MOVIE_API_BASE_URL']
+base_url = None
 
-Movie = movie.Movie
+def configure_request(app):
+  global api_key, base_url
+  api_key = app.config['MOVIE_API_KEY']
+  base_url = app.config['MOVIE_API_BASE_URL']
 
 def get_movies(category):
   '''
@@ -63,7 +64,7 @@ def get_movie(id):
     movie_details_data = url.read()
     movie_details_response = json.loads(movie_details_data)
     movie_object = None
-    
+
     if movie_details_response:
       id = movie_details_response.get('id')
       title = movie_details_response.get('original_title')
@@ -74,3 +75,18 @@ def get_movie(id):
       movie_object = Movie(id, title, overview, poster, vote_average, vote_count)
 
     return movie_object
+
+def search_movie(movie_name):
+  search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key, movie_name)
+
+  with urllib.request.urlopen(search_movie_url) as url:
+    search_movie_data = url.read()
+    search_movie_response = json.loads(search_movie_data)
+
+    search_movie_results = None
+
+    if search_movie_response['results']:
+      search_movie_list = search_movie_response['results']
+      search_movie_results = process_results(search_movie_list)
+  
+  return search_movie_results
